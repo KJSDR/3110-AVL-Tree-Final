6d9d143ac3bc269344843ba1976683fb523c4267
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-02-25 22:23:23 -0800

    added notes as per instructions

--- src/avl_node.py
+++ src/avl_node.py
@@ -4,7 +4,10 @@ class RecipeNode:
     
     Each node stores a recipes title as the ket which is used for the ordering and balancing
     and it also has the full recipes data with the ingreditents and instructions to make
-    
+
+    AVL attributes:
+    height: its used to calculate balance factor and it trigger rotations
+    left/right: the child pointers
     """
 
     def __init__(self, title: str, ingredients: list, instructions: str = ""):
