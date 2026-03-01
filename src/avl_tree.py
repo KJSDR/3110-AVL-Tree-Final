b88850329147957ac23b1a322067198bc0e9bcf7
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-02-28 19:32:43 -0800

    added function to get the height and also sort in order

--- src/avl_tree.py
+++ src/avl_tree.py
@@ -21,8 +21,17 @@ class AVLTree:
         return self._search(self.root, title.lower())
 
     def search_prefix(self, prefix: str) -> list:
-        """
-        """
+        """return all recipes who titles start with the prefix you give like auto complete and stuff"""
         results = []
         self._search_prefix(self.root, prefix.lower(), results)
         return results
+        
+    def inorder(self) -> list:
+        """return all recipes in alphabet order by title"""
+        result = []
+        self._inorder(self.root, result)
+        return result
+
+    def get_height(self) -> int:
+        """return the height of the tree"""
+        return self._get_height(self.root)
